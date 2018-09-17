73c85043f43733c2a126977a06f476217b75046e
Adriaan de Jonge <adriaandejonge@gmail.com>
2018-09-17 17:03:02 +0800

    Option to store cookies across requests

--- loadtest.go
+++ loadtest.go
@@ -5,15 +5,20 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"os"
 	"strconv"
 	"strings"
 	"time"
+
+	"golang.org/x/net/publicsuffix"
 )
 
 var reportInterval int
 var baseURL string
 var filters []string
+var keepCookies bool
 
 func main() {
 	fileName := flag.String("l", "", "file name of access log file to interpret")
@@ -21,18 +26,21 @@ func main() {
 	filtersArg := flag.String("f", "", "comma-separated list of URLs to filter")
 	repIntArg := flag.Int("r", 1, "report interval in seconds")
 	basUrlArg := flag.String("b", "", "base URL as prefix in front of paths in access logs")
+	keepCookiesArg := flag.Bool("k", false, "boolean value keep cookies or not")
 
 	flag.Parse()
 
 	filters = strings.Split(*filtersArg, ",")
 	reportInterval = *repIntArg
 	baseURL = *basUrlArg
+	keepCookies = *keepCookiesArg
 
 	log.Println("Access log file:", *fileName)
 	log.Println("Concurrent req #:", *goroutines)
 	log.Println("Filters:", filters)
 	log.Printf("Report every: %d seconds", reportInterval)
 	log.Println("Base URL", baseURL)
+	log.Println("Keep cookies", keepCookies)
 
 	queue := make(chan string)
 	timer := make(chan int)
@@ -79,35 +87,51 @@ func report(hits chan int, timer chan int) {
 
 func readFromQueue(id int, queue chan string, stop chan struct{}, hits chan int) {
 
+	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := &http.Client{
+		Jar: jar,
+	}
+
 	for {
-
-		processLogLine(<-queue, hits)
-
+		processLogLine(<-queue, hits, client)
 	}
 }
 
-func processLogLine(logLine string, hits chan int) {
+func processLogLine(logLine string, hits chan int, client *http.Client) {
 
 	start := strings.Index(logLine, "\"GET ")
 	end := strings.Index(logLine, " HTTP/")
 
 	if start > 0 && end > 0 {
-		url := logLine[start+5 : end]
+		path := logLine[start+5 : end]
 
 		filtered := false
 
 		for _, el := range filters {
-			if strings.Index(url, el) > 0 {
+			if strings.Index(path, el) > 0 {
 				filtered = true
 			}
 		}
 
 		if !filtered {
-			_, err := http.Get(baseURL + url)
+
+			r, err := client.Get(baseURL + path)
+
 			if err != nil {
 				log.Println(err)
 			} else {
 				hits <- 1
+				respURL, err := url.Parse(baseURL + path)
+				if err != nil {
+					log.Println("Could not read cookies")
+					log.Fatal(err)
+				} else if keepCookies {
+					client.Jar.SetCookies(respURL, r.Cookies())
+				}
 			}
 		}
 	}
